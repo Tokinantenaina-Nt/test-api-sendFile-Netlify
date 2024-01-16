a9f961c42e63ac7dab1b3fbc02395f7f0055aa6a
Tokinantenaina <tokib2oba@yahoo.com>
2024-01-16 21:52:24 +0300

    ajout de fs

--- functions/app.js
+++ functions/app.js
@@ -2,10 +2,20 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.get('*', (req, res) => {
     const filePath = path.join(process.cwd(), "functions/test.json");
-    res.sendFile(filePath);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur lors de la lecture du fichier JSON');
+        } else {
+            // Envoi du contenu JSON en tant que réponse
+            res.json(JSON.parse(data));
+        }
+    });
+    //res.sendFile(filePath);
 });
 app.listen(6200, () => {
     console.log('listen 6200');
