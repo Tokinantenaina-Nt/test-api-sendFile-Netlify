d74791ffad356b9fafbe868eaed7b936315c636e
Tokinantenaina <tokib2oba@yahoo.com>
2024-01-16 21:58:59 +0300

    read synchrone

--- functions/app.js
+++ functions/app.js
@@ -6,15 +6,14 @@ const fs = require('fs');
 
 app.get('*', (req, res) => {
     const filePath = path.join(process.cwd(), "functions/test.json");
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Erreur lors de la lecture du fichier JSON');
-        } else {
-            // Envoi du contenu JSON en tant que réponse
-            res.json(JSON.parse(data));
-        }
-    });
+    //OU, lecture synchrone du fichier (à utiliser avec prudence, car cela peut bloquer le serveur)
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        res.json(JSON.parse(data));
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur lors de la lecture du fichier JSON');
+    }
     //res.sendFile(filePath);
 });
 app.listen(6200, () => {
