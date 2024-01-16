97e68de1a63ddd1fa37a447c443da379cf4ae337
Tokinantenaina <tokib2oba@yahoo.com>
2024-01-16 20:57:38 +0300

    edit path.join

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,11 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/Capture.PNG')
+    const filePath = path.join(process.cwd(), "functions/Capture.PNG");
+    res.sendFile(filePath);
 });
 app.listen(6200, () => {
     console.log('listen 6200');
