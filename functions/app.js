f0dff289fdfafe3b998006886d0938a156e48e0d
Tokinantenaina <tokib2oba@yahoo.com>
2024-01-16 22:01:32 +0300

    res.sendFile

--- functions/app.js
+++ functions/app.js
@@ -6,15 +6,15 @@ const fs = require('fs');
 
 app.get('*', (req, res) => {
     const filePath = path.join(process.cwd(), "functions/test.json");
-    //OU, lecture synchrone du fichier (à utiliser avec prudence, car cela peut bloquer le serveur)
-    try {
-        const data = fs.readFileSync(filePath, 'utf8');
-        res.json(JSON.parse(data));
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erreur lors de la lecture du fichier JSON');
-    }
-    //res.sendFile(filePath);
+    // //OU, lecture synchrone du fichier (à utiliser avec prudence, car cela peut bloquer le serveur)
+    // try {
+    //     const data = fs.readFileSync(filePath, 'utf8');
+    //     res.json(JSON.parse(data));
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).send('Erreur lors de la lecture du fichier JSON');
+    // }
+    res.sendFile(filePath);
 });
 app.listen(6200, () => {
     console.log('listen 6200');
