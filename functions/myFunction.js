c47a40c8e9e8678a1464c303f0b88d1da5dee4fa
Tokinantenaina-Nt <tokib2oba@yahoo.com>
2024-01-17 15:32:53 +0300

    depuis le debut

---
+++ functions/myFunction.js
@@ -0,0 +1,7 @@
+// myFunction.js
+exports.handler = async function (event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello from Netlify Edge Function!" }),
+    };
+};
