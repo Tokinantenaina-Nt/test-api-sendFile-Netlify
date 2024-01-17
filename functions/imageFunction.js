1cf18d6bf380f050c3f56a8e15947aae20206c11
Tokinantenaina-Nt <tokib2oba@yahoo.com>
2024-01-17 16:07:41 +0300

    path

--- functions/imageFunction.js
+++ functions/imageFunction.js
@@ -1,23 +1,35 @@
 // imageProcessingFunction.js
 const sharp = require('sharp');
+const path = require('path');
 const fs = require('fs');
 
 exports.handler = async function (event, context) {
-    // Lire l'image depuis le fichier
-    const imagePath = './Capture.png';
-    const imageBuffer = fs.readFileSync(imagePath);
+    // Obtenir le chemin complet de l'image en fonction du répertoire de déploiement de Netlify
+    const imagePath = path.join(__dirname, 'Capture.png');
 
-    // Traitement de l'image (redimensionnement dans cet exemple)
-    const processedImageBuffer = await sharp(imageBuffer)
-        .resize({ width: 300 })
-        .toBuffer();
+    try {
+        // Lire l'image depuis le fichier
+        const imageBuffer = fs.readFileSync(imagePath);
 
-    return {
-        statusCode: 200,
-        body: processedImageBuffer.toString('base64'),
-        isBase64Encoded: true,
-        headers: {
-            'Content-Type': 'image/png',
-        },
-    };
+        // Traitement de l'image (redimensionnement dans cet exemple)
+        const processedImageBuffer = await sharp(imageBuffer)
+            .resize({ width: 300 })
+            .toBuffer();
+
+        return {
+            statusCode: 200,
+            body: processedImageBuffer.toString('base64'),
+            isBase64Encoded: true,
+            headers: {
+                'Content-Type': 'image/png',
+            },
+        };
+    } catch (error) {
+        console.error('Error reading or processing image:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
 };
